efd2ac05db079c14e33d3555e81cf6ab192c2d5d
Victor <westlundvictor8@gmail.com>
2021-10-12 22:34:32 +0200

    link to add game if u are site mod

--- src/GameList.js
+++ src/GameList.js
@@ -3,16 +3,26 @@ import { APIContext } from "./APIContext";
 import { Link } from "react-router-dom";
 import Loading from "./Loading";
 import GameListEntry from "./Components/GameListEntry";
+import { UserContext } from "./UserContext";
 import "./Css/GameList.css";
 
 function GameList() {
   const [listOfGames, setListOfGames] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [isSiteModerator,setIsSiteModerator] = useState(false)
+  const username = useContext(UserContext);
+
 
   const API = useContext(APIContext);
 
   async function fetchListOfGames() {
     let response = await fetch(`${API}/api/Game`);
+    response = response.json();
+    return response;
+  }
+
+  async function fetchIsSiteModerator() {
+    let response = await fetch(`${API}/Login`,{credentials: "include"});
     response = response.json();
     return response;
   }
@@ -29,6 +39,13 @@ function GameList() {
       setListOfGames(data);
     });
 
+    let siteMod = fetchIsSiteModerator();
+
+    siteMod.then((data) => {
+      setIsSiteModerator(data.siteModerator)
+      console.log(data);
+    }) 
+
     console.log(games);
   }, []);
 
@@ -40,6 +57,7 @@ function GameList() {
 
   return (
     <div id="game-list">
+      {isSiteModerator && <h1>Länk för att lägga till spel</h1>}
       {loading && <Loading />}
       {listOfGamesJSX}
     </div>
