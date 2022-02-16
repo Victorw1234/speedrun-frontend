0395dd0773f5cd57598aa81d4f944c285a960669
Victor <westlundvictor8@gmail.com>
2022-02-16 17:05:21 +0100

    Fix

--- src/Components/GameListEntry.js
+++ src/Components/GameListEntry.js
@@ -1,20 +1,38 @@
-import React,{useContext} from 'react'
-import { Link } from 'react-router-dom'
-import '../Css/GameListEntry.css'
-import { APIContext } from '../APIContext';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import "../Css/GameListEntry.css";
+import { APIContext } from "../APIContext";
+import { Card, CardMedia, CardContent, Typography } from "@material-ui/core";
 
-function GameListEntry({title,titleUrl,imageName}) {
+function GameListEntry({ title, titleUrl, imageName }) {
     const API = useContext(APIContext);
     return (
-        
-            <div id="game-list-entry" className="card">
+        <Link
+            style={{ textDecoration: "none", boxShadow: "none" }}
+            to={`/Game/${titleUrl}`}
+        >
+            <Card style={{ margin: "12px" }}>
+                <CardMedia>
+                    <img
+                        alt="halo3.jpg"
+                        src={`${API}/Resources/Images/${imageName}`}
+                        style={{ width: "300px", height: "300px" }}
+                    />
+                </CardMedia>
+                <CardContent>
+                    <Typography align="center">{title}</Typography>
+                </CardContent>
+            </Card>
+        </Link>
+    );
+}
+
+export default GameListEntry;
+{
+    /*<div id="game-list-entry" className="card">
                 <Link to={`/Game/${titleUrl}`}>
                     <img alt="halo3.jpg" src={`${API}/Resources/Images/${imageName}`}/>
                     <p>{title}</p>
                 </Link>
-            </div>
-        
-    )
+    </div>*/
 }
-
-export default GameListEntry
